ba0fa5665a64e57fd3fc98840bf736546dcac2b8
MortyQA <zebekow@gmail.com>
2020-04-03 20:16:47 +0300

    @Задание4_Готово

---
+++ addressbook_testing2/addressbook_testing2/AddNewHelper.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_testing2
+{
+        public class ContactName
+        {
+            private string first_name;
+            private string middle_name = "";
+            private string last_name = "";
+            private string nick_name = "";
+
+            public ContactName(string first_name)
+            {
+            this.first_name = first_name;
+            }
+
+            public string First_name
+            {
+                get
+                {
+                    return first_name;
+                }
+                set
+                {
+                    first_name = value;
+                }
+            }
+
+            public string Middle_name
+            {
+                get
+                {
+                    return middle_name;
+                }
+                set
+                {
+                    middle_name = value;
+                }
+            }
+
+            public string Last_name
+            {
+                get
+                {
+                    return last_name;
+                }
+                set
+                {
+                    last_name = value;
+                }
+            }
+
+            public string Nick_name
+            {
+                get
+                {
+                    return nick_name;
+                }
+                set
+                {
+                    nick_name = value;
+                }
+            }
+        }
+
+        public class ContactWork
+        {
+            private string contact_title;
+            private string contact_company = "";
+
+            public ContactWork(string contact_title)
+            {
+            this.contact_title = contact_title;
+            }
+   
+            public string Contact_title
+            {
+                get
+                {
+                    return contact_title;
+                }
+                set
+                {
+                    contact_title = value;
+                }
+            }
+
+            public string Contact_company
+            {
+                get
+                {
+                    return contact_company;
+                }
+                set
+                {
+                    contact_company = value;
+                }
+            }
+        }
+
+        public class ContactAddress
+        {
+            private string address;
+
+            public ContactAddress(string address)
+            {
+            this.address = address;
+            }
+
+            public string Address
+            {
+                get
+                {
+                    return address;
+                }
+                set
+                {
+                    address = value;
+                }
+            }
+        }
+
+        public class ContactPhone
+        {
+            private string homephone;
+            private string mobilephone = "";
+            private string workphone = "";
+            private string faxphone = "";
+
+            public ContactPhone(string homephone)
+            {
+            this.homephone = homephone;
+            }
+
+            public string Homephone
+            {
+                get
+                {
+                    return homephone;
+                }
+                set
+                {
+                    homephone = value;
+                }
+            }
+
+            public string Mobilephone
+            {
+                get
+                {
+                    return mobilephone;
+                }
+                set
+                {
+                    mobilephone = value;
+                }
+            }
+
+            public string Workphone
+            {
+                get
+                {
+                    return workphone;
+                }
+                set
+                {
+                    workphone = value;
+                }
+            }
+
+            public string Faxphone
+            {
+                get
+                {
+                    return faxphone;
+                }
+                set
+                {
+                    faxphone = value;
+                }
+            }
+
+        }
+        public class ContactHomepage
+        {
+            private string contact_hpage;
+
+            public ContactHomepage(string contact_hpage)
+            {
+            this.contact_hpage = contact_hpage;
+            }
+
+            public string Contact_hpage
+            {
+                get
+                {
+                    return contact_hpage;
+                }
+                set
+                {
+                    contact_hpage = value;
+                }
+            }
+        }
+        public class ContactEmail
+        {
+            private string contact_email;
+            private string contact_email2 = "";
+            private string contact_email3 = "";
+
+            public ContactEmail(string contact_email)
+            {
+            this.contact_email = contact_email;
+            }
+
+
+
+            public string Contact_email
+            {
+                get
+                {
+                    return contact_email;
+                }
+                set
+                {
+                    contact_email = value;
+                }
+            }
+
+            public string Contact_email2
+            {
+                get
+                {
+                    return contact_email;
+                }
+                set
+                {
+                    contact_email = value;
+                }
+            }
+
+            public string Contact_email3
+            {
+                get
+                {
+                    return contact_email;
+                }
+                set
+                {
+                    contact_email = value;
+                }
+            }
+
+        }
+        public class ContactSecondary
+        {
+            private string contact_address2;
+            private string contact_phone2 = "";
+            private string contact_notes = "";
+
+            public ContactSecondary(string contact_address2)
+            {
+            this.contact_address2 = contact_address2;
+            }
+
+            public string Contact_address2
+            {
+                get
+                {
+                    return contact_address2;
+                }
+                set
+                {
+                    contact_address2 = value;
+                }
+            }
+            public string Contact_phone2
+            {
+                get
+                {
+                    return contact_phone2;
+                }
+                set
+                {
+                    contact_phone2 = value;
+                }
+            }
+            public string Contact_notes
+            {
+                get
+                {
+                    return contact_notes;
+                }
+                set
+                {
+                    contact_notes = value;
+                }
+            }
+
+
+        }
+
+
+    }
+//}
--- addressbook_testing2/addressbook_testing2/GroupCreationTests.cs
+++ addressbook_testing2/addressbook_testing2/GroupCreationTests.cs
@@ -54,6 +54,140 @@ namespace addressbook_testing2
             ReturnToGroupPage();
             LogOut();
         }
+        [Test]
+        public void NewContact()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToAddNew();
+            ContactName cname = new ContactName("First name");
+            cname.Middle_name = "Middle name";
+            cname.Last_name = "Last name";
+            cname.Nick_name = "Nick name";
+            ContactNameInformation(cname);
+            AddContact();
+            LogOut();
+        }
+
+        private void AddContact()
+        {
+            driver.FindElement(By.XPath("(//input[@name='submit'])[2]")).Click();
+        }
+
+        private void ContactSecondary(ContactSecondary secondary)
+        {
+            driver.FindElement(By.Name("address2")).Click();
+            driver.FindElement(By.Name("address2")).Clear();
+            driver.FindElement(By.Name("address2")).SendKeys(secondary.Contact_address2);
+            driver.FindElement(By.Name("phone2")).Click();
+            driver.FindElement(By.Name("phone2")).Clear();
+            driver.FindElement(By.Name("phone2")).SendKeys(secondary.Contact_phone2);
+            driver.FindElement(By.Name("notes")).Click();
+            driver.FindElement(By.Name("notes")).Clear();
+            driver.FindElement(By.Name("notes")).SendKeys(secondary.Contact_notes);
+        }
+
+        private void SelectContactGroup()
+        {
+            driver.FindElement(By.Name("new_group")).Click();
+            new SelectElement(driver.FindElement(By.Name("new_group"))).SelectByText("aaa");
+        }
+
+        private void ContactBirthday()
+        {
+            driver.FindElement(By.Name("bday")).Click();
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText("1");
+            driver.FindElement(By.Name("bday")).Click();
+            driver.FindElement(By.Name("bmonth")).Click();
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText("January");
+            driver.FindElement(By.Name("bmonth")).Click();
+            driver.FindElement(By.Name("byear")).Click();
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys("2020");
+            driver.FindElement(By.Name("aday")).Click();
+            new SelectElement(driver.FindElement(By.Name("aday"))).SelectByText("4");
+            driver.FindElement(By.Name("aday")).Click();
+            driver.FindElement(By.Name("amonth")).Click();
+            new SelectElement(driver.FindElement(By.Name("amonth"))).SelectByText("March");
+            driver.FindElement(By.Name("amonth")).Click();
+            driver.FindElement(By.Name("ayear")).Click();
+            driver.FindElement(By.Name("ayear")).Clear();
+            driver.FindElement(By.Name("ayear")).SendKeys("2020");
+        }
+
+        private void ContactHomePage(ContactHomepage chpage)
+        {
+            driver.FindElement(By.Name("homepage")).Click();
+            driver.FindElement(By.Name("homepage")).Clear();
+            driver.FindElement(By.Name("homepage")).SendKeys(chpage.Contact_hpage);
+        }
+
+        private void ContactEmailInformation(ContactEmail cemail)
+        {
+            driver.FindElement(By.Name("email")).Click();
+            driver.FindElement(By.Name("email")).Clear();
+            driver.FindElement(By.Name("email")).SendKeys(cemail.Contact_email);
+            driver.FindElement(By.Name("email2")).Click();
+            driver.FindElement(By.Name("email2")).Clear();
+            driver.FindElement(By.Name("email2")).SendKeys(cemail.Contact_email2);
+            driver.FindElement(By.Name("email3")).Click();
+            driver.FindElement(By.Name("email3")).Clear();
+            driver.FindElement(By.Name("email3")).SendKeys(cemail.Contact_email3);
+        }
+
+        private void ContactPhoneInformation(ContactPhone cphone)
+        {
+            driver.FindElement(By.Name("home")).Click();
+            driver.FindElement(By.Name("home")).Clear();
+            driver.FindElement(By.Name("home")).SendKeys(cphone.Homephone);
+            driver.FindElement(By.Name("mobile")).Click();
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys(cphone.Mobilephone);
+            driver.FindElement(By.Name("work")).Click();
+            driver.FindElement(By.Name("work")).Clear();
+            driver.FindElement(By.Name("work")).SendKeys(cphone.Workphone);
+            driver.FindElement(By.Name("fax")).Click();
+            driver.FindElement(By.Name("fax")).Clear();
+            driver.FindElement(By.Name("fax")).SendKeys(cphone.Faxphone);
+        }
+
+        private void ContactAddress(ContactAddress caddress)
+        {
+            driver.FindElement(By.Name("address")).Click();
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys(caddress.Address);
+        }
+
+        private void ContactWorkInformation(ContactWork cwork)
+        {
+            driver.FindElement(By.Name("title")).Click();
+            driver.FindElement(By.Name("title")).Clear();
+            driver.FindElement(By.Name("title")).SendKeys(cwork.Contact_title);
+            driver.FindElement(By.Name("company")).Click();
+            driver.FindElement(By.Name("company")).Clear();
+            driver.FindElement(By.Name("company")).SendKeys(cwork.Contact_company);
+        }
+
+        private void ContactNameInformation(ContactName contact_name)
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact_name.First_name);
+            driver.FindElement(By.Name("middlename")).Click();
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(contact_name.Middle_name);
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact_name.Last_name);
+            driver.FindElement(By.Name("nickname")).Click();
+            driver.FindElement(By.Name("nickname")).Clear();
+            driver.FindElement(By.Name("nickname")).SendKeys(contact_name.Nick_name);
+        }
+
+        private void GoToAddNew()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
 
         private void LogOut()
         {
